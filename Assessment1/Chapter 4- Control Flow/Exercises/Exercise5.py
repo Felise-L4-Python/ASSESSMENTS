3560d7892f9e81aac047afc5eaacf350f3995268
Felise-L4-Python <145339926+Felise-L4-Python@users.noreply.github.com>
2024-01-18 01:04:13 +0400

    Exercise5.py

--- Assessment1/Chapter 4- Control Flow/Exercises/Exercise5.py
+++ Assessment1/Chapter 4- Control Flow/Exercises/Exercise5.py
@@ -1,17 +1,22 @@
-
+#The variable becomes a list with 3 favorite fruits 
 favorite_fruits= ['Mango', 'Cantaloupe Melon', 'Cherry']
 
+#If Mango is in the list then it will print the statement
 if 'Mango' in favorite_fruits:
     print ("You really like Mangoes!")
 
+#If Cantaloupe Melon is in the list then it will print the statement
 if 'Cantaloupe Melon' in favorite_fruits:
     print ("You really like Cantoloupe Melons!")
 
+#If Cherry is in the list then it will print the statement
 if 'Cherry' in favorite_fruits:
     print ("You really like Cherries!")
 
+#If Watermelon is in the list then it will print the statement
 if 'Watermelon' in favorite_fruits:
     print ("You really like Watermelons!")
 
+#If Guava is in the list then it will print the statement
 if 'Guava' in favorite_fruits:
     print ("You really like Guavas!")
