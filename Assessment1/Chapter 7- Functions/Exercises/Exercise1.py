c7d969ec4ec89dfa567d480412df903a800c8c92
Felise-L4-Python <145339926+Felise-L4-Python@users.noreply.github.com>
2024-01-18 10:11:09 +0400

    Exercise1.py

--- Assessment1/Chapter 7- Functions/Exercises/Exercise1.py
+++ Assessment1/Chapter 7- Functions/Exercises/Exercise1.py
@@ -1,5 +1,5 @@
 def display_message(): #This defines the variable "display_message"
-    msg = "I'm learning to program codes using Python" #The variable ' 
+    msg = "I'm learning to program codes using Python" #The variable is assigned to the statement
     print(msg) #prints the variable "msg"
 
 display_message() #runs the code stored in "display_message"
